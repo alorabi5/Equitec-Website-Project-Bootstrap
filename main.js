634751f9160f7c613c7cbb9373f166f22e1a15e1
Redha Aloraibi <reda9808@gmail.com>
2025-06-21 23:04:38 +0300

    full page section rebuild

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-$(".slide-section").slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-});
+const clientsLocations = document.querySelectorAll(".our-clients-locations ul li");
+const clientLogosList = document.querySelectorAll(".our-clients-logos-container");
+
+const clientsRegion = (event) => {
+        AOS.init();
+
+    const selectedLocationElement = event.target;
+    const selectedClientsLocationList = document.querySelector(`.our-clients-logos-container.${selectedLocationElement.className}`);
+
+    if(selectedLocationElement && selectedClientsLocationList){
+        clientsLocations.forEach(title => title.classList.remove("active-title-clients-option"));
+        // clientLogosList.forEach(logo => logo.style.display = "none");
+        clientLogosList.forEach(logo => logo.classList.add("hidden-clients"));
+
+        
+        selectedLocationElement.classList.add("active-title-clients-option");
+        // selectedClientsLocationList.style.display = "block";
+        selectedClientsLocationList.classList.remove("hidden-clients");
+        // console.log(selectedClientsLocationList)
+    }
+}
--- slick.js
+++ slick.js
@@ -1,1 +1,36 @@
-$(".testSlick").slick();
+$(".slide-section").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+});
+
+$(".our-clients-logos-group").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 1300,
+            settings: {
+            slidesToShow: 4,
+
+            }
+        },
+        {
+            breakpoint: 1100,
+            settings: {
+            slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        },
+    ]
+});
+
